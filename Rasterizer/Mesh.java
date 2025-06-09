f68822530f19ecfd471af19996bb7ee0d98c81b5
Sigurd Hoel Lundestad <prettyshitluacoder@gmail.com>
2025-06-10 00:15:45 +0200

    add mesh class
    Created a mesh class which handles a list of triangles

---
+++ Rasterizer/Mesh.java
@@ -0,0 +1,27 @@
+package rasterizer;
+
+import java.util.ArrayList;
+
+public class Mesh {
+    private ArrayList<Triangle> triangles;
+
+    /**
+     * Creates a mesh based on the specified triangles.
+     * 
+     * @param triangles a list of triangles
+     */
+    public Mesh(ArrayList<Triangle> triangles) {
+        this.triangles = triangles;
+    }
+
+
+    /**
+     * Returs a list of all the triangles of which this mesh is based on.
+     * 
+     * @return a list of tirangles within this mesh
+     */
+    public ArrayList<Triangle> getTriangles() {
+        return triangles;
+    }
+
+}
--- Rasterizer/Triangle.java
+++ Rasterizer/Triangle.java
@@ -1,3 +1,12 @@
+package rasterizer;
+
 import java.util.Vector;
 
+/**
+ * Creates a triangle defined by the vectors first, second and third.
+ * 
+ * @param first first vector of triangle
+ * @param second second vector of triangle
+ * @param third third vector of triangle
+ */
 public record Triangle(Vector<Float> first, Vector<Float> second, Vector<Float> third) {};
